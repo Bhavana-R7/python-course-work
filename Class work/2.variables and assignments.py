91ff41d63a7108cbbd10dd94415d15eaf3cd3853
Bhavana-R7 <ramabatthinabhavana@gmail.com>
2025-07-01 23:32:44 +0530

    variable and assignments

--- Class work/2.variables and assignments.py
+++ Class work/2.variables and assignments.py
@@ -1,2 +1,40 @@
 #2.Variable Rules And Assignments
 myvar=10
+print(myvar)
+Myvar=10
+print(Myvar)
+myvar1=10
+print(myvar1)
+_myvar=10
+print(_myvar)
+MYvar=10
+print(MYvar)
+#case sensitivity
+myvar=10
+print(myvar)
+myvar=20
+print(myvar)
+#Multiple assignments
+a,b,c=100,200,300
+print(a)
+print(b)
+print(c)
+
+#same value Assignment
+a=b=c=100
+print(a)
+print(b)
+print(c)
+
+#swapping variable
+a=100
+b=50
+a,b=b,a
+print(a)
+print(b)
+
+#deleting asssignmemts
+a=10
+b=20
+del (a)
+print(b)
