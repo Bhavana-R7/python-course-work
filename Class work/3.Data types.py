bbfed1359e7951adddbd85e1413af3f568409aa4
Bhavana-R7 <ramabatthinabhavana@gmail.com>
2025-07-02 10:50:24 +0530

    data types

---
+++ Class work/3.Data types.py
@@ -0,0 +1,76 @@
+#integer type
+a=20
+b=30
+print(a,b)
+print(type(a))
+print(type(b))
+
+#floating point
+a=2.3
+b=4.3
+print(a,b)
+print(type(a))
+print(type(b))
+
+#complex value
+x=4+6j
+y=2+4j
+print(x,y)
+print(type(x))
+print(type(b))
+
+#string type
+m='hai'
+n="hello"
+o='''hlo'''
+print(m,n,o)
+print(type(m))
+print(type(n))
+print(type(0))
+
+#list
+i=list()
+i=[1,2,3,4,5,6]
+print(i)
+print(type(i))
+
+#tuple type
+t=tuple()
+p=(1,2,3,4)
+print(p)
+print(type(p))
+
+#set type
+s=set()
+m={10,20,30,40}
+print(m)
+print(type(m))
+
+#frozen set 
+z=frozenset({10,20,30,40})
+print(z)
+print(type(z))
+
+#dictionary type
+d=dict()
+g={'name':"bhavana" ,'age':"21"}
+print(g)
+print(type(g))
+
+#boolean type
+u=True
+v=False
+print(u,v)
+print(type(u))
+print(type(v))
+
+#none type
+d=None
+print(d)
+print(type(d))
+
+
+
+
+
+
